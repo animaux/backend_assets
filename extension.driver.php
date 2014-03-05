1dd29b73e4c531419db05f132077728ae62d37cc
Nils Hörrmann <github@nilshoerrmann.de>
2014-03-05 13:45:09 +0100

    Rename folder

--- extension.driver.php
+++ extension.driver.php
@@ -19,13 +19,13 @@
 		public function appendAssets() {
 			
 			// Styles
-			$styles = URL . '/workspace/assets-backend/css/backend.css';
+			$styles = URL . '/workspace/backend-assets/css/backend.css';
 			if(file_exists($styles)) {
 				Administration::instance()->Page->addStylesheetToHead($styles, 'screen', 1000, false);
 			}
 			
 			// Scripts
-			$scripts = URL . '/workspace/assets-backend/js/backend.js';
+			$scripts = URL . '/workspace/backend-assets/js/backend.js';
 			if(file_exists($styles)) {
 				Administration::instance()->Page->addScriptToHead($scripts', 1001);
 			}
